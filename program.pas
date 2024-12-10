ca7760c5a710e90017ce494712d91ae211f190a7
RiverioSumbayak <riverio.sby@gmail.com>
2024-12-10 10:41:19 +0700

    7th commit - fixing porgram.pas

--- program.pas
+++ program.pas
@@ -2,20 +2,20 @@ program ShowroomMobil;
 uses crt;
 
 type
-  // Definisi record untuk mobil
+
     Mobil = record
         merk: string;
         harga: real;
     end;
 
-  // Array untuk menyimpan data mobil berdasarkan jenisnya
+
     JenisMobil = record
         namaJenis: string;
         daftarMobil: array[1..3] of Mobil;
     end;
 
 var
-    showroom: array[1..4] of JenisMobil; // 4 jenis mobil
+    showroom: array[1..4] of JenisMobil; 
     pilihanJenis, pilihanMerk: integer;
     skemaPembayaran: string;
     lamaCicilan: integer;
